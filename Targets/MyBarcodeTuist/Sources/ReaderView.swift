8f76316c05cd70c4e439e289f9f01d11360e2242
Hyunyou <hyunyou.lim@samsung.com>
2022-05-22 22:00:18 +0900

    Barcode Recognition App

---
+++ Targets/MyBarcodeTuist/Sources/ReaderView.swift
@@ -0,0 +1,164 @@
+//
+//  ReaderView.swift
+//  BarcodeTest
+//
+//  Created by Hyunyou Lim on 2022/04/04.
+//
+
+import UIKit
+import AVFoundation
+
+enum ReaderStatus {
+    case success(_ code: String?)
+    case fail
+    case stop(_ isButtonTap: Bool)
+}
+
+protocol ReaderViewDelegate: AnyObject {
+    func readerComplete(status: ReaderStatus)
+}
+
+class ReaderView: UIView{
+    weak var delegate: ReaderViewDelegate?
+    
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var centerGuideLineView: UIView?
+    
+    var captureSession: AVCaptureSession?
+    
+    //get만 있는 computed property
+    var isRunning: Bool {
+        guard let captureSession = self.captureSession else {
+            return false
+        }
+
+        return captureSession.isRunning
+    }
+    
+    let metadataObjectTypes: [AVMetadataObject.ObjectType] = [.upce, .code39, .code39Mod43, .code93, .code128, .ean8, .ean13, .aztec, .pdf417, .itf14, .dataMatrix, .interleaved2of5]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.initialSetupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.initialSetupView()
+    }
+    
+    private func initialSetupView(){
+        self.clipsToBounds = true
+        self.captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch let error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        guard let captureSession = self.captureSession else {
+            self.fail()
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput){
+            captureSession.addInput(videoInput)
+        } else {
+            self.fail()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metadataOutput){
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = self.metadataObjectTypes
+        } else {
+            self.fail()
+            return
+        }
+    
+        setPreviewLayer()
+        setCenterGuideLineView()
+    }
+    
+    //카메라 preview layer를 view위에 얹기
+    private func setPreviewLayer() {
+        guard let captureSession = self.captureSession else {
+            return
+        }
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.frame = self.layer.bounds
+        
+        self.layer.addSublayer(previewLayer)
+        
+        self.previewLayer = previewLayer
+    }
+    
+    //guideline layer
+    private func setCenterGuideLineView() {
+        let centerGuideLineView = UIView()
+        centerGuideLineView.translatesAutoresizingMaskIntoConstraints = false
+        //color literal
+        centerGuideLineView.backgroundColor = #colorLiteral(red: 0, green: 0.4456313776, blue: 0, alpha: 1)
+        self.addSubview(centerGuideLineView)
+        self.bringSubviewToFront(centerGuideLineView)
+        
+        centerGuideLineView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        centerGuideLineView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        centerGuideLineView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        centerGuideLineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        self.centerGuideLineView = centerGuideLineView
+    }
+}
+
+extension ReaderView {
+    func start(){
+        self.captureSession?.startRunning()
+    }
+    
+    func stop(isButtonTap: Bool){
+        self.captureSession?.stopRunning()
+        
+        self.delegate?.readerComplete(status: .stop(isButtonTap))
+    }
+    
+    func fail(){
+        self.delegate?.readerComplete(status: .fail)
+        self.captureSession = nil
+    }
+    
+    func found(code: String){
+        self.delegate?.readerComplete(status: .success(code))
+    }
+}
+
+extension ReaderView: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        stop(isButtonTap: false)
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject,
+                  let stringValue = readableObject.stringValue else {
+                      return
+                  }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+    }
+}
